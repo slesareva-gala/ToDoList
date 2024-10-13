71e0613ef82138f9782f6b46cf35c89a6e4f3fb4
Галина Слесарева <galaslesareva@gmail.com>
2024-10-14 01:22:51 +0300

    add День 4 helmet и 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom"
 export const NotFound = () => {
     return (
         <div className="container">
-            <h1>Not Found</h1>
-            <Link to="/">to Home</Link>
+            <div className="notFound">
+                Not Found
+                <Link to="/" className="toHome">go Home</Link>
+            </div>
+
+
+
         </div>
     )
 }
